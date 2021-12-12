d3dc4ab320415b85e2c03ffca29dcf77a03c5f25
Mihail Nechaev <sadovni4ek@gmail.com>
2021-12-12 22:38:07 +0300

    task modified

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,8 +4,8 @@ function solveEquation(a, b, c) {
   let arr;
    
   const dis = b**2 - 4 * a * c;
-  let x2 = (-b - Math.sqrt(arr)) / (2 * a);
-  let x1 = (-b + Math.sqrt(arr)) / (2 * a);
+  let x2 = (-b - Math.sqrt(dis)) / (2 * a);
+  let x1 = (-b + Math.sqrt(dis)) / (2 * a);
   if (dis < 0) {
     arr = [];
   } else if (dis == 0) {
