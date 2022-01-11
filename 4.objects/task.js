5fae318c55ae80763b46c72d18e53ec4a7cbad3a
Mihail Nechaev <sadovni4ek@gmail.com>
2022-01-11 21:36:35 +0300

    task completed?

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let John = new Student("John", "male", 25);
+let Bruce = new Student("Bruce", "male", 38);
+let Diana = new Student("Diana", "female", 17);
+let Geralt = new Student("Geralt from Rivia", "mutant", 125);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = new Array();
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+};
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (...theMarks) {
+  if (this.marks === undefined) {
+    this.marks = new Array();
+    this.marks.push(...theMarks);
+  } else {
+    this.marks.push(...theMarks);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  if (this.marks === undefined) {
+    this.marks = new Array();
+  }
+  console.log(this.marks);
+
+  let sum = 0;
+  for (mark of this.marks) {
+    sum += mark;
+  }
+  let lgth = this.marks.length;
+  return sum / lgth;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
+}
