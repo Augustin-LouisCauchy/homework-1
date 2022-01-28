42158c94d9641d191550cdd929bcd7e593496d49
Mihail Nechaev <sadovni4ek@gmail.com>
2022-01-28 22:34:03 +0300

    All task completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -21,3 +21,45 @@ function validateCount(parseValue) {
 }
 
 // TASK 2______________________________________________________
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  getArea() {
+    let semiPerimeter = this.getPerimeter() / 2;
+    let squareCalc = Math.sqrt(
+      semiPerimeter *
+        (semiPerimeter - this.a) *
+        (semiPerimeter - this.b) *
+        (semiPerimeter - this.c)
+    );
+    let square = squareCalc.toFixed(3);
+    return parseFloat(square);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (err) {
+    return {
+      getPerimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      getArea() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
